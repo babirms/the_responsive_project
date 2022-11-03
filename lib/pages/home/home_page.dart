cbe1687516be646968d6882466a8e626791713cc
Bárbara Ramos <bramos@id.uff.br>
2022-11-03 11:13:49 -0300

    add: create HomePage page and MobileAppBar widget

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:the_responsive_project/pages/home/widgets/app_bar/mobile_app_bar.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: ((context, constraints) {
+        return Scaffold(
+          appBar: constraints.maxWidth < 800
+              ? PreferredSize(
+                  child: MobileAppBar(),
+                  preferredSize: Size(double.infinity, 56))
+              : AppBar(),
+          drawer: Drawer(),
+        );
+      }),
+    );
+  }
+}
---
+++ lib/pages/home/widgets/app_bar/mobile_app_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MobileAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(' Flutter'),
+        centerTitle: true,
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart)),
+        ]);
+  }
+}
