be428370e27fbd5f928bd71ba436327b53b77a45
Bárbara Ramos <bramos@id.uff.br>
2022-11-04 18:56:26 -0300

    add: create CourseItem

---
+++ lib/pages/home/widgets/course_item.dart
@@ -0,0 +1,36 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+
+class CourseItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+      Image.network(
+        'https://images.unsplash.com/photo-1661961110372-8a7682543120?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+        fit: BoxFit.fitWidth,
+      ),
+      const SizedBox(
+        height: 4,
+      ),
+      const Flexible(
+        child: AutoSizeText(
+          'Criação de Apps Android e iOS com flutter - Crise 16 Apps',
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 15),
+        ),
+      ),
+      const Text(
+        'Daniel Ciolfi',
+        style: TextStyle(color: Colors.grey),
+      ),
+      const Text(
+        'R\$22,90',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 14,
+          color: Colors.white,
+        ),
+      ),
+    ]);
+  }
+}
--- lib/pages/home/widgets/sections/courses_section.dart
+++ lib/pages/home/widgets/sections/courses_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:the_responsive_project/breakpoints.dart';
+import 'package:the_responsive_project/pages/home/widgets/course_item.dart';
 
 class CoursesSection extends StatelessWidget {
   @override
@@ -8,8 +9,8 @@ class CoursesSection extends StatelessWidget {
       return GridView.builder(
           gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 300,
-            crossAxisSpacing: 4,
-            mainAxisSpacing: 4,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
           ),
           physics: const NeverScrollableScrollPhysics(),
           shrinkWrap: true,
@@ -18,7 +19,7 @@ class CoursesSection extends StatelessWidget {
               vertical: 16,
               horizontal: constraints.maxWidth >= tabletBreakpoint ? 0 : 16),
           itemBuilder: (context, index) {
-            return Container(color: Colors.red);
+            return CourseItem();
           });
     });
   }
